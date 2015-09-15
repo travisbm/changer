fee31b363e74e1ab1ba5a8c9b7be55eaa18cfb1e
Travis Montgomery <travisbm@gmail.com>
2015-09-15 10:21:09 -0500

    working, needs final tests and refactoring

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,5 +1,44 @@
 class Changer
-  def self.run(amt)
+  attr_accessor :quarters, :dimes, :nickels, :pennies
 
+  def print
+    puts "Q: #{@quarters} D: #{@dimes} N: #{@nickels} P: #{@pennies}"
+  end
+
+  def quarters(amt)
+    if amt.modulo(25).zero?
+      @quarters = amt / 25
+    else
+      remainder = amt % 25
+      @quarters = amt / 25
+    end
+    # return @quarters
+    return remainder
+  end
+
+  def dimes(amt)
+    if amt.modulo(10).zero?
+      @dimes = amt / 10
+    else
+      remainder = amt % 10
+      @dimes = amt / 10
+    end
+    #return @dimes
+    return remainder
+  end
+
+  def nickels(amt)
+    if amt.modulo(5).zero?
+      @nickels = amt / 5
+    else
+      remainder = amt % 5
+      @nickels = amt / 5
+    end
+    #return @nickels
+    return remainder
+  end
+
+  def pennies(amt)
+    @pennies = nickels(dimes(quarters(amt)))
   end
 end
--- spec/changer_test.rb
+++ spec/changer_test.rb
@@ -8,8 +8,32 @@ class ChangerTest < MiniTest::Test
     assert(Changer)
   end
 
-  def test_run_works
-    assert_respond_to(Changer, :run)
+  # def test_run_works
+  #   assert_respond_to(Changer, :run)
+  # end
+
+  def test_quarters
+    ch = Changer.new
+    assert(@quarters = 3, ch.quarters(75))
+    assert(@quarters = 2, ch.quarters(74))
+  end
+
+  def test_dimes
+    ch = Changer.new
+    assert(@dimes = 7, ch.dimes(75))
+    assert(@dimes = 8, ch.dimes(80))
+  end
+
+  def test_nickels
+    ch = Changer.new
+    assert(@nickels = 15, ch.nickels(75))
+    assert(@nickels = 0, ch.nickels(3))
+    assert(@nickels = 1, ch.nickels(7))
+  end
+
+  def test_pennies
+    ch = Changer.new
+    assert(@pennies = 3, ch.pennies(73))
   end
 
 end
